1c8a8cc53c4338b183c8fd1d7ea17ecd52943625
Kacper Trzciński <siema311@gmail.com>
2022-04-03 23:14:03 +0200

    auth module setup

---
+++ backend/src/auth/auth.controller.ts
@@ -0,0 +1,23 @@
+import { Body, Controller, Post } from '@nestjs/common';
+import { AuthService } from './auth.service';
+import { SignUpDto } from './dto';
+import { Tokens } from './types';
+
+@Controller('auth')
+export class AuthController {
+  constructor(private authService: AuthService) {}
+
+  @Post('/local/signup')
+  signupLocal(@Body() dto: SignUpDto): Promise<Tokens> {
+    this.authService.signupLocal(dto);
+  }
+
+  @Post('/local/signin')
+  signinLocal() {}
+
+  @Post('/logout')
+  logout() {}
+
+  @Post('/refresh')
+  refreshTokens() {}
+}
