4e22ab962aadb5af439559a0ad7f9136d4a24cab
Kacper Trzciński <siema311@gmail.com>
2022-04-04 12:56:46 +0200

    create at and rt strategies

---
+++ backend/src/auth/strategies/at.strategy.ts
@@ -0,0 +1,19 @@
+import { Injectable } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
+import { PassportStrategy } from '@nestjs/passport';
+import { ExtractJwt, Strategy } from 'passport-jwt';
+
+@Injectable()
+export class AtStrategy extends PassportStrategy(Strategy, 'jwt') {
+  constructor(config: ConfigService) {
+    super({
+      jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
+      secretOrKey: config.get('AT_SECRET'),
+    });
+  }
+
+  validate(payload: any) {
+    //req.user = payload
+    return payload;
+  }
+}
