41537d37309b7c147473ea8cc7469c98874571cc
Kacper Trzciński <siema311@gmail.com>
2022-04-05 17:18:16 +0200

    logic behind tokens in cookies

--- backend/src/auth/auth.controller.ts
+++ backend/src/auth/auth.controller.ts
@@ -4,9 +4,12 @@ import {
   HttpCode,
   HttpStatus,
   Post,
+  Req,
+  Res,
   UseGuards,
 } from '@nestjs/common';
-import { GetCurrentUser } from 'src/common/decorators';
+import { Response, Request } from 'express';
+import { GetCurrentUser, RtCookie } from 'src/common/decorators';
 import { AtGuard, RtGuard } from 'src/common/guards';
 import { AuthService } from './auth.service';
 import { SignInDto, SignUpDto } from './dto';
@@ -18,27 +21,67 @@ export class AuthController {
 
   @Post('local/signup')
   @HttpCode(HttpStatus.CREATED)
-  signupLocal(@Body() dto: SignUpDto): Promise<Tokens> {
-    return this.authService.signupLocal(dto);
+  async signupLocal(@Body() dto: SignUpDto, @Res() response: Response) {
+    const tokens = await this.authService.signupLocal(dto);
+    this.asignCookies(response, tokens);
+    return response.json({ successful: true });
   }
 
   @Post('local/signin')
   @HttpCode(HttpStatus.OK)
-  signinLocal(@Body() dto: SignInDto): Promise<Tokens> {
-    return this.authService.signinLocal(dto);
+  async signinLocal(@Body() dto: SignInDto, @Res() response: Response) {
+    const tokens = await this.authService.signinLocal(dto);
+    this.asignCookies(response, tokens);
+    return response.json({ successful: true });
   }
 
   @UseGuards(AtGuard)
   @Post('logout')
   @HttpCode(HttpStatus.OK)
-  logout(@GetCurrentUser() user: Payload): Promise<void> {
-    return this.authService.logout(user.sub);
+  async logout(@GetCurrentUser() user: Payload, @Res() response: Response) {
+    await this.authService.logout(user.sub);
+    this.deteleCookies(response);
+    return response.json({ successful: true });
   }
 
   @UseGuards(RtGuard)
   @Post('refresh')
   @HttpCode(HttpStatus.OK)
-  refreshTokens(@GetCurrentUser() user: PayloadWithRt): Promise<Tokens> {
-    return this.authService.refreshTokens(user.sub, user.refreshToken);
+  async refreshTokens(
+    @GetCurrentUser() user: Payload,
+    @Res() response: Response,
+    @RtCookie() rtCookie: string,
+  ) {
+    const tokens = await this.authService.refreshTokens(user.sub, rtCookie);
+    this.asignCookies(response, tokens);
+    return response.json({ successful: true });
+  }
+
+  asignCookies(response: Response, tokens: Tokens) {
+    response.cookie('at', tokens.access_token, {
+      expires: new Date(new Date().getTime() + 60 * 1000 * 30), // 30 min
+      sameSite: 'lax',
+      httpOnly: true,
+    });
+
+    response.cookie('rt', tokens.refresh_token, {
+      expires: new Date(new Date().getTime() + 60 * 1000 * 60 * 24 * 7), // 7 days
+      sameSite: 'lax',
+      httpOnly: true,
+    });
+  }
+
+  deteleCookies(response: Response) {
+    response.cookie('at', 'DELETED', {
+      expires: new Date(), // expires in the moment of creation
+      sameSite: 'lax',
+      httpOnly: true,
+    });
+
+    response.cookie('rt', 'DELETED', {
+      expires: new Date(), // expires in the moment of creation
+      sameSite: 'lax',
+      httpOnly: true,
+    });
   }
 }
--- backend/src/auth/auth.service.ts
+++ backend/src/auth/auth.service.ts
@@ -76,9 +76,7 @@ export class AuthService {
 
   async refreshTokens(userId: number, rt: string): Promise<Tokens> {
     const user = await this.prisma.user.findUnique({
-      where: {
-        id: userId,
-      },
+      where: { id: userId },
     });
 
     if (!user) {
