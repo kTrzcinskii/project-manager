e6948cb078af555dc4ca3eae699a7f66ce80cb9e
Kacper Trzciński <siema311@gmail.com>
2022-04-05 17:16:05 +0200

    simplify guards

--- backend/src/common/guards/at.guard.ts
+++ backend/src/common/guards/at.guard.ts
@@ -1,26 +1,9 @@
-import { ExecutionContext, Injectable } from '@nestjs/common';
-import { Reflector } from '@nestjs/core';
+import { Injectable } from '@nestjs/common';
 import { AuthGuard } from '@nestjs/passport';
-import { Observable } from 'rxjs';
 
 @Injectable()
 export class AtGuard extends AuthGuard('jwt') {
-  constructor(private reflector: Reflector) {
+  constructor() {
     super();
-  }
-
-  canActivate(
-    context: ExecutionContext,
-  ): boolean | Promise<boolean> | Observable<boolean> {
-    const isPublic = this.reflector.getAllAndOverride('isPublic', [
-      context.getHandler(),
-      context.getClass(),
-    ]);
-
-    if (isPublic) {
-      return true;
-    }
-
-    return super.canActivate(context);
   }
 }
