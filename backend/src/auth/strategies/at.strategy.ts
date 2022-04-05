e91adb7ad86b66bd1d109e255039594d69a58307
Kacper Trzciński <siema311@gmail.com>
2022-04-05 17:16:23 +0200

    switch to cookies strategy

--- backend/src/auth/strategies/at.strategy.ts
+++ backend/src/auth/strategies/at.strategy.ts
@@ -1,20 +1,25 @@
 import { Injectable } from '@nestjs/common';
 import { ConfigService } from '@nestjs/config';
 import { PassportStrategy } from '@nestjs/passport';
-import { ExtractJwt, Strategy } from 'passport-jwt';
+import { Request } from 'express';
+import { Strategy } from 'passport-jwt';
 import { Payload } from '../types';
 
 @Injectable()
 export class AtStrategy extends PassportStrategy(Strategy, 'jwt') {
   constructor(config: ConfigService) {
     super({
-      jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
+      jwtFromRequest: (req: Request) => {
+        if (!req || !req.cookies) return null;
+        return req.cookies['at'];
+      },
       secretOrKey: config.get('AT_SECRET'),
     });
   }
 
   validate(payload: Payload) {
     //req.user = payload
+
     return payload;
   }
 }
--- backend/src/auth/strategies/rt.strategy.ts
+++ backend/src/auth/strategies/rt.strategy.ts
@@ -2,25 +2,24 @@ import { Injectable } from '@nestjs/common';
 import { ConfigService } from '@nestjs/config';
 import { PassportStrategy } from '@nestjs/passport';
 import { Request } from 'express';
-import { ExtractJwt, Strategy } from 'passport-jwt';
+import { Strategy } from 'passport-jwt';
 import { Payload } from '../types';
 
 @Injectable()
 export class RtStrategy extends PassportStrategy(Strategy, 'jwt-refresh') {
   constructor(config: ConfigService) {
     super({
-      jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
+      jwtFromRequest: (req: Request) => {
+        if (!req || !req.cookies) return null;
+        return req.cookies['rt'];
+      },
       secretOrKey: config.get('RT_SECRET'),
-      passReqToCallback: true,
     });
   }
 
-  validate(req: Request, payload: Payload) {
+  validate(payload: Payload) {
     //req.user = payload
-    const refreshToken = req.get('authorization').replace('Bearer', '').trim();
-    return {
-      ...payload,
-      refreshToken,
-    };
+
+    return payload;
   }
 }
