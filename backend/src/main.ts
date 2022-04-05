bcc1da10a39fceea9c78f01fdd467726b0779973
Kacper Trzciński <siema311@gmail.com>
2022-04-05 17:15:41 +0200

    enable cors and cookies

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,14 +1,22 @@
 import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import * as cookieParser from 'cookie-parser';
+import { ConfigService } from '@nestjs/config';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  const config = app.get(ConfigService);
   app.useGlobalPipes(
     new ValidationPipe({
       whitelist: true,
     }),
   );
+  app.enableCors({
+    credentials: true,
+    origin: config.get('FRONTEND_ORIGIN'),
+  });
+  app.use(cookieParser());
   await app.listen(5000);
 }
 bootstrap();
