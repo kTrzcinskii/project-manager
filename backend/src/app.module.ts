bdd62e067d44e6927fdb1ba9020fad4af6cd800e
Kacper Trzciński <siema311@gmail.com>
2022-04-07 15:18:11 +0200

    project module init

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -3,8 +3,9 @@ import { AuthModule } from './auth/auth.module';
 import { PrismaModule } from './prisma/prisma.module';
 import { UserModule } from './user/user.module';
 import { CookiesModule } from './cookies/cookies.module';
+import { ProjectModule } from './project/project.module';
 @Module({
-  imports: [AuthModule, PrismaModule, UserModule, CookiesModule],
+  imports: [AuthModule, PrismaModule, UserModule, CookiesModule, ProjectModule],
   controllers: [],
   providers: [],
 })
