ce0870af405781829bad3fc52dc7635871b362c6
Kacper Trzciński <siema311@gmail.com>
2022-04-08 15:49:23 +0200

    create project func

--- backend/src/project/project.controller.ts
+++ backend/src/project/project.controller.ts
@@ -1,4 +1,20 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Get, Patch, Post, UseGuards } from '@nestjs/common';
+import { Payload } from 'src/auth/types';
+import { GetCurrentUser } from 'src/common/decorators';
+import { AtGuard } from 'src/common/guards';
+import { CreateProjectDto } from './dto';
+import { ProjectService } from './project.service';
 
-@Controller('project')
-export class ProjectController {}
+@UseGuards(AtGuard)
+@Controller('projects')
+export class ProjectController {
+  constructor(private projectService: ProjectService) {}
+
+  @Post('create')
+  createProject(
+    @GetCurrentUser() user: Payload,
+    @Body() dto: CreateProjectDto,
+  ) {
+    return this.projectService.createProject(user.sub, dto);
+  }
+}
--- backend/src/project/project.service.ts
+++ backend/src/project/project.service.ts
@@ -1,4 +1,41 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, InternalServerErrorException } from '@nestjs/common';
+import { PrismaService } from 'src/prisma/prisma.service';
+import { CreateProjectDto } from './dto';
 
 @Injectable()
-export class ProjectService {}
+export class ProjectService {
+  constructor(private prisma: PrismaService) {}
+
+  async createProject(userId: number, dto: CreateProjectDto) {
+    const project = await this.prisma.project.create({
+      data: {
+        title: dto.title,
+        description: dto.description,
+        deadline: new Date(dto.deadline),
+        prority: dto.priority,
+        goals: {
+          createMany: {
+            data: [...dto.goals],
+          },
+        },
+        user: {
+          connect: {
+            id: userId,
+          },
+        },
+      },
+    });
+
+    if (!project) {
+      throw new InternalServerErrorException(
+        "We couldn't add your project to the database, try again later",
+      );
+    }
+
+    const projectGoals = await this.prisma.goal.findMany({
+      where: { projectId: project.id },
+    });
+
+    return { ...project, projectGoals };
+  }
+}
