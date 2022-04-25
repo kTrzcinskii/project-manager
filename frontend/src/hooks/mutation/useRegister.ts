89c753c489758d20c475558dee9f8652a73c6641
Kacper Trzciński <siema311@gmail.com>
2022-04-25 15:58:09 +0200

    useRegister hook

---
+++ frontend/src/hooks/mutation/useRegister.ts
@@ -0,0 +1,14 @@
+import { AxiosError, AxiosResponse } from "axios";
+import { useMutation } from "react-query";
+import registerAPI from "../../api/register";
+import IRegisterFormValues from "../../interfaces/IRegisterFormValues";
+import IServerErrorResponse from "../../interfaces/IServerErrorResponse";
+import ISuccessful from "../../interfaces/ISuccessful";
+
+export default function useRegister() {
+  return useMutation<
+    AxiosResponse<ISuccessful, any>,
+    Error | AxiosError<IServerErrorResponse>,
+    IRegisterFormValues
+  >(async (values: IRegisterFormValues) => await registerAPI(values));
+}
