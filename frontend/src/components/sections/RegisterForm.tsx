3cf5b8da2fa934e3f4d85b38eb1a832c671a5e30
Kacper Trzciński <siema311@gmail.com>
2022-04-25 15:59:02 +0200

    use mutation in register  form

--- frontend/src/components/sections/RegisterForm.tsx
+++ frontend/src/components/sections/RegisterForm.tsx
@@ -6,6 +6,9 @@ import InputField from "../ui/InputField";
 import PasswordInputField from "../ui/PasswordInputField";
 import { FaUser } from "react-icons/fa";
 import IRegisterFormValues from "../../interfaces/IRegisterFormValues";
+import useRegister from "../../hooks/mutation/useRegister";
+import axios from "axios";
+import axiosInstance from "../../utils/axiosInstance";
 
 const RegisterForm: React.FC = () => {
   const initialValues: IRegisterFormValues = {
@@ -16,30 +19,47 @@ const RegisterForm: React.FC = () => {
 
   const formSpacing = useBreakpointValue({ base: 4, md: 6, lg: 8 });
 
+  const mutation = useRegister();
+
   return (
     <Formik
       initialValues={initialValues}
       onSubmit={(values, action) => {
         console.log(values);
+        mutation.mutate(values);
+        if (mutation.isError) {
+          //TODO: Error Handling
+          if (axios.isAxiosError(mutation.error)) {
+            console.log(mutation.error.response?.data);
+          }
+        } else {
+          //TODO: Action after successful register
+        }
       }}
       validationSchema={RegisterFormSchema}
     >
-      {({ isSubmitting }) => (
-        <Form>
+      {({ isSubmitting, values, handleSubmit }) => (
+        <Form onSubmit={handleSubmit}>
           <VStack w='full' align='flex-start' spacing={formSpacing}>
             <InputField
               name='username'
               type='text'
               placeholder='Username'
               icon={<FaUser color='#285E61' />}
+              value={values.username}
             />
             <InputField
               name='email'
               type='email'
               placeholder='Email'
               icon={<AtSignIcon color='teal.700' />}
+              value={values.email}
             />
-            <PasswordInputField name='password' placeholder='Password' />
+            <PasswordInputField
+              name='password'
+              placeholder='Password'
+              value={values.password}
+            />
             <Button
               type='submit'
               colorScheme='teal'
