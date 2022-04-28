8e3b23a1cc0511da9bf4b3487478091b11914ace
Kacper Trzciński <siema311@gmail.com>
2022-04-28 16:45:47 +0200

    added response type

--- frontend/src/api/login.ts
+++ frontend/src/api/login.ts
@@ -1,11 +1,12 @@
 import { AxiosAuthRefreshRequestConfig } from "axios-auth-refresh";
 import ILoginFormValues from "../interfaces/ILoginFormValues";
+import ISuccessful from "../interfaces/ISuccessful";
 import axiosInstance from "../utils/axiosInstance";
 
 const endpoint = "/auth/local/signin";
 
 const loginAPI = (values: ILoginFormValues) => {
-  return axiosInstance.post(endpoint, values, {
+  return axiosInstance.post<ISuccessful>(endpoint, values, {
     skipAuthRefresh: true,
   } as AxiosAuthRefreshRequestConfig);
 };
