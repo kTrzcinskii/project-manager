8eb7b59b7f7528514181f41b1e8af45183b6819a
Kacper Trzciński <siema311@gmail.com>
2022-04-28 16:45:03 +0200

    IMe interface

--- frontend/src/api/meServerSide.ts
+++ frontend/src/api/meServerSide.ts
@@ -1,10 +1,11 @@
 import { AxiosAuthRefreshRequestConfig } from "axios-auth-refresh";
+import IMe from "../interfaces/IMe";
 import axiosInstance from "../utils/axiosInstance";
 
 const endpoint = "/user/me";
 
 const meServerSideAPI = (cookies: string) => {
-  return axiosInstance.get(endpoint, {
+  return axiosInstance.get<IMe>(endpoint, {
     withCredentials: true,
     headers: { Cookie: cookies },
     skipAuthRefresh: true,
---
+++ frontend/src/interfaces/IMe.ts
@@ -0,0 +1,8 @@
+interface IMe {
+  id: number;
+  email: string;
+  username: string;
+  createdAt: string;
+}
+
+export default IMe;
