95b4a4a8e95e0db3a07ba4c95b746c9071c2a779
Kacper Trzciński <siema311@gmail.com>
2022-05-07 18:47:47 +0200

    add possibility to add ref to InputField

--- frontend/src/components/ui/form/InputField.tsx
+++ frontend/src/components/ui/form/InputField.tsx
@@ -7,15 +7,16 @@ import {
   InputProps,
 } from "@chakra-ui/react";
 import { useField } from "formik";
-import { InputHTMLAttributes, ReactNode } from "react";
+import { InputHTMLAttributes, LegacyRef, ReactNode } from "react";
 
 type InputFieldProps = InputHTMLAttributes<HTMLInputElement> &
   InputProps & {
     name: string;
     icon?: ReactNode;
+    myRef?: LegacyRef<HTMLInputElement> | undefined;
   };
 
-const InputField: React.FC<InputFieldProps> = ({ icon, ...props }) => {
+const InputField: React.FC<InputFieldProps> = ({ icon, myRef, ...props }) => {
   const [field, meta] = useField(props);
 
   return (
@@ -28,6 +29,7 @@ const InputField: React.FC<InputFieldProps> = ({ icon, ...props }) => {
           id={field.name}
           variant='filled'
           _focus={{ borderColor: "teal.500", borderWidth: 2 }}
+          ref={myRef}
         />
       </InputGroup>
       <FormErrorMessage>{meta.error}</FormErrorMessage>
