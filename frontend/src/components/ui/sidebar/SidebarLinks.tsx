0f89b3878e6219aa073057d80b1531cc146168a1
Kacper Trzciński <siema311@gmail.com>
2022-05-11 22:43:41 +0200

    add stats to sidebar links

--- frontend/src/components/ui/sidebar/SidebarLinks.tsx
+++ frontend/src/components/ui/sidebar/SidebarLinks.tsx
@@ -3,6 +3,7 @@ import { BiHome } from "react-icons/bi";
 import { MdFavoriteBorder } from "react-icons/md";
 import { FiSettings } from "react-icons/fi";
 import { BsCalendarX, BsCalendarCheck, BsCalendarEvent } from "react-icons/bs";
+import { ImStatsDots } from "react-icons/im";
 
 interface SidebarLinkProps {
   text: string;
@@ -28,6 +29,7 @@ const SidebarLinks: Array<SidebarLinkProps> = [
     link: "/in_progress",
   },
   { text: "Backlog", icon: BsCalendarX, link: "/backlog" },
+  { text: "Statistics", icon: ImStatsDots, link: "/statistics" },
   { text: "Settings", icon: FiSettings, link: "/settings" },
 ];
 
