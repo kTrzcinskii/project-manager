a30e82dfdb5d7fbff5578bb492ce26303b337b8e
Kacper Trzciński <siema311@gmail.com>
2022-05-11 22:59:39 +0200

    stats module init

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -5,8 +5,9 @@ import { UserModule } from './user/user.module';
 import { CookiesModule } from './cookies/cookies.module';
 import { ProjectModule } from './project/project.module';
 import { GoalModule } from './goal/goal.module';
+import { StatsModule } from './stats/stats.module';
 @Module({
-  imports: [AuthModule, PrismaModule, UserModule, CookiesModule, ProjectModule, GoalModule],
+  imports: [AuthModule, PrismaModule, UserModule, CookiesModule, ProjectModule, GoalModule, StatsModule],
   controllers: [],
   providers: [],
 })
