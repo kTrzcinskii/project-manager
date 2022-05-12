efb2acfde1c06d412cb7294ec482be1d5f52b21f
Kacper Trzciński <siema311@gmail.com>
2022-05-12 20:42:32 +0200

    finished get main stats func

--- backend/src/stats/stats.service.ts
+++ backend/src/stats/stats.service.ts
@@ -12,23 +12,60 @@ export class StatsService {
 
     const { searchFrom, searchTo } = this.getDateRange(from);
 
-    const createdProjectsNumber = (
-      await this.prisma.project.findMany({
-        where: {
-          userId,
-          createdAt: {
-            gte: searchFrom,
-            lte: searchTo,
-          },
+    const createdProjectsNumber = await this.prisma.project.count({
+      where: {
+        userId,
+        createdAt: {
+          gte: searchFrom,
+          lte: searchTo,
         },
-      })
-    ).length;
-
-    const completedProjectsNumber = await this.prisma.project.findMany({
-      where: {},
+      },
     });
 
-    return { createdProjectsNumber };
+    const completedProjectsNumber = await this.prisma.project.count({
+      where: { userId, completedAt: { gte: searchFrom, lte: searchTo } },
+    });
+
+    const updatedProjectsNumber = await this.prisma.project.count({
+      where: { userId, updatedAt: { gte: searchFrom, lte: searchTo } },
+    });
+
+    const userProjectsIds = (
+      await this.prisma.user.findUnique({
+        where: { id: userId },
+        select: { projects: { select: { id: true } } },
+      })
+    ).projects.map((project) => project.id);
+
+    const createdGoalsNumber = await this.prisma.goal.count({
+      where: {
+        projectId: { in: userProjectsIds },
+        createdAt: { gte: searchFrom, lte: searchTo },
+      },
+    });
+
+    const completedGoalsNumber = await this.prisma.goal.count({
+      where: {
+        projectId: { in: userProjectsIds },
+        completedAt: { gte: searchFrom, lte: searchTo },
+      },
+    });
+
+    const updatedGoalsNumber = await this.prisma.goal.count({
+      where: {
+        projectId: { in: userProjectsIds },
+        updatedAt: { gte: searchFrom, lte: searchTo },
+      },
+    });
+
+    return {
+      createdProjectsNumber,
+      completedProjectsNumber,
+      updatedProjectsNumber,
+      createdGoalsNumber,
+      completedGoalsNumber,
+      updatedGoalsNumber,
+    };
   }
 
   getDateRange(from: from | number) {
