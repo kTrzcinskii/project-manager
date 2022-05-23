63ef6b82618c49d700922db954b2812a952f8a52
Kacper Trzciński <siema311@gmail.com>
2022-05-23 22:57:42 +0200

    change date type to string

--- frontend/src/interfaces/IFilterFormValues.ts
+++ frontend/src/interfaces/IFilterFormValues.ts
@@ -3,15 +3,15 @@ export type priorityType = "all" | "low" | "medium" | "high";
 export interface IFilterFormValues {
   favorite?: boolean;
   priority: priorityType;
-  title?: "string";
-  deadlineFrom?: Date;
-  deadlineTo?: Date;
-  createdFrom?: Date;
-  createdTo?: Date;
-  updatedFrom?: Date;
-  updatedTo?: Date;
-  completedFrom?: Date;
-  completedTo?: Date;
+  title?: string;
+  deadlineFrom?: string;
+  deadlineTo?: string;
+  createdFrom?: string;
+  createdTo?: string;
+  updatedFrom?: string;
+  updatedTo?: string;
+  completedFrom?: string;
+  completedTo?: string;
   progressBarFrom?: number;
   progressBarTo?: number;
 }
