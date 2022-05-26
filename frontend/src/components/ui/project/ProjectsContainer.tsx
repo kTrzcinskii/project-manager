17b5aea96c35c37d01571dc68ca3a55d5f53c5f5
Kacper Trzciński <siema311@gmail.com>
2022-05-26 23:17:32 +0200

    project container init

--- frontend/src/components/ui/project/ProjectsContainer.tsx
+++ frontend/src/components/ui/project/ProjectsContainer.tsx
@@ -1,54 +1,41 @@
-import { HStack, VStack, Text, Box, Container } from "@chakra-ui/react";
-import { ReactNode, useEffect, useState } from "react";
-import transfromMonthNumberToString from "../../../utils/transfromMonthNumberToString";
-import FilterBar from "./FilterBar";
-
+import useGetProjects from "../../../hooks/query/useGetProjects";
+import { Flex, Grid, Spinner } from "@chakra-ui/react";
 interface ProjectsContainerProps {
-  title: "Projects" | "Finished" | "In Progress" | "Backlog";
-  children?: ReactNode;
+  page: number;
+  query: string;
 }
 
 const ProjectsContainer: React.FC<ProjectsContainerProps> = ({
-  children,
-  title,
+  page,
+  query,
 }) => {
-  const [date, setDate] = useState("");
-  useEffect(() => {
-    const today = new Date();
-    const year = today.getFullYear();
-    const month = transfromMonthNumberToString(today.getMonth());
-    setDate(`${month}, ${year}`);
-  }, []);
+  const { data, isLoading, isError } = useGetProjects(page, query);
 
-  return (
-    <VStack
-      h='full'
-      alignItems='flex-start'
-      w='90%'
-      bgColor='white'
-      borderRadius='lg'
-      justifyContent='flex-start'
-      flex={1}
-    >
-      <Container maxW='container.lg'>
-        <HStack
-          justifyContent='space-between'
-          w='full'
-          mt={{ base: 5, md: 8, lg: 10 }}
-          fontWeight='semibold'
-          fontSize={{ base: "lg", md: "xl", lg: "2xl" }}
-          px={5}
-        >
-          <Text>{title}</Text>
-          <Text>{date}</Text>
-        </HStack>
-        <Box w='full'>
-          <FilterBar />
-        </Box>
-        {children}
-      </Container>
-    </VStack>
-  );
+  if (isError) {
+    return <></>;
+  }
+
+  if (isLoading) {
+    return (
+      <Flex
+        w='full'
+        mx='auto'
+        justifyContent='center'
+        alignItems='center'
+        mt={20}
+      >
+        <Spinner
+          thickness='5px'
+          speed='0.65s'
+          emptyColor='gray.200'
+          color='teal.500'
+          boxSize={100}
+        />
+      </Flex>
+    );
+  }
+
+  return <Grid></Grid>;
 };
 
 export default ProjectsContainer;
