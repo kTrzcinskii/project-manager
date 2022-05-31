3cad9f8e5799b2586ab4b69aefeafb34c01d6b05
Kacper Trzciński <siema311@gmail.com>
2022-05-31 13:01:32 +0200

    0 projects creadet message

--- frontend/src/components/ui/project/ProjectsContainer.tsx
+++ frontend/src/components/ui/project/ProjectsContainer.tsx
@@ -1,7 +1,16 @@
-import { Box, Flex, Grid, GridItem, Heading, Text } from "@chakra-ui/react";
+import {
+  Box,
+  Button,
+  Flex,
+  Grid,
+  GridItem,
+  Heading,
+  Text,
+} from "@chakra-ui/react";
 import LoadingSpinner from "../utils/LoadingSpinner";
 import ProjectCard from "./ProjectCard";
 import IAllProjects from "../../../interfaces/IAllProjects";
+import { useRouter } from "next/router";
 interface ProjectsContainerProps {
   page: number;
   query: string;
@@ -17,6 +26,8 @@ const ProjectsContainer: React.FC<ProjectsContainerProps> = ({
   isError,
   isLoading,
 }) => {
+  const router = useRouter();
+
   if (isError) {
     return (
       <Box pt={{ base: 90, md: 160, lg: 200 }} w='80%' mx='auto'>
@@ -53,9 +64,34 @@ const ProjectsContainer: React.FC<ProjectsContainerProps> = ({
   }
 
   if (data?.projects.length === 0) {
-    //TODO
     if (!query) {
-      return <>USER HAS 0 PROJECTS - create project btn</>;
+      return (
+        <Flex
+          w='full'
+          mx='auto'
+          justifyContent='center'
+          alignItems='center'
+          mt={{ base: 20, md: 40, lg: 60 }}
+          flexDir='column'
+        >
+          <Text
+            textAlign='center'
+            color='gray.800'
+            fontSize={{ base: "md", md: "lg", lg: "xl" }}
+          >
+            It looks like you haven&apos;t created any project yet - let&apos;s
+            change it!
+          </Text>
+          <Button
+            mt={5}
+            colorScheme='teal'
+            _focus={{ ring: 3, ringColor: "teal.700" }}
+            onClick={() => router.push("/create-project")}
+          >
+            Create New Project
+          </Button>
+        </Flex>
+      );
     }
     //TODO
     return (
