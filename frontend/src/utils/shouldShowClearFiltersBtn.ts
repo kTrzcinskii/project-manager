9cc1d2cce73a2ad118b357594b6f7ef032528389
Kacper Trzciński <siema311@gmail.com>
2022-06-05 13:39:50 +0200

    check if query contains filters

---
+++ frontend/src/utils/shouldShowClearFiltersBtn.ts
@@ -0,0 +1,20 @@
+const shouldShowClearFiltersBtns = (query: string): boolean => {
+  if (!query) {
+    return false;
+  }
+
+  const globalRegexp = new RegExp("&\\w+=[^&]+", "igm");
+
+  const fields = query.match(globalRegexp);
+  if (!fields) {
+    return false;
+  }
+  const newFields = fields.map((field) => field.split("=")[0].slice(1));
+  if (newFields.length === 1 && newFields[0] === "srt") {
+    return false;
+  }
+
+  return true;
+};
+
+export default shouldShowClearFiltersBtns;
