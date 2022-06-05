c76b06fbb9998b4a588110555728273dc5a7e703
Kacper Trzciński <siema311@gmail.com>
2022-06-05 23:28:49 +0200

    in-progress page

---
+++ frontend/pages/in-progress.tsx
@@ -0,0 +1,54 @@
+import { VStack } from "@chakra-ui/react";
+import type { NextPage, NextPageContext } from "next";
+import Sidebar from "../src/components/sections/Sidebar";
+import Header from "../src/components/ui/home/Header";
+import ProjectsWrapper from "../src/components/ui/project/ProjectsWrapper";
+import IMe from "../src/interfaces/IMe";
+import minHonPagesWithSidebar from "../src/utils/minHonPagesWithSidebar";
+import isUserLoggedIn from "../src/utils/server-side/isUserLoggedIn";
+import redirectServerSide from "../src/utils/server-side/redirectServerSide";
+import setCookiesServerSide from "../src/utils/server-side/setCookiesServerSide";
+
+const InProgress: NextPage<{
+  user: IMe;
+}> = ({ user }) => {
+  const minH = minHonPagesWithSidebar;
+
+  return (
+    <Sidebar>
+      <VStack spacing={{ base: 5, md: 10 }} minH={minH}>
+        <Header
+          username={user.username}
+          constantText="that's what you're currently working on!"
+        />
+        <ProjectsWrapper
+          title='Projects In Progress'
+          query='&status=inProgress'
+          showFavoriteFilter={true}
+        />
+      </VStack>
+    </Sidebar>
+  );
+};
+
+export async function getServerSideProps(ctx: NextPageContext) {
+  let cookies = ctx.req?.headers.cookie;
+
+  if (!cookies) {
+    return redirectServerSide("/unauthorized");
+  }
+
+  if (!cookies.includes("at=") && cookies.includes("rt=")) {
+    cookies = await setCookiesServerSide(ctx, cookies);
+  }
+
+  const { logged, user } = await isUserLoggedIn(cookies);
+
+  if (!logged || !user) {
+    return redirectServerSide("/unauthorized");
+  }
+
+  return { props: { user } };
+}
+
+export default InProgress;
