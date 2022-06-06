1f5c8f30327020a84e7a37b740a967d37fb8f045
Kacper Trzciński <siema311@gmail.com>
2022-06-06 15:14:45 +0200

    fix bug

--- frontend/src/api/getMainStats.ts
+++ frontend/src/api/getMainStats.ts
@@ -2,7 +2,7 @@ import axiosInstance from "../utils/axiosInstance";
 import IMainStats from "../interfaces/IMainStats";
 
 const getMainStats = async (query?: string) => {
-  const addToEndpoit = query ? "/" + query : "";
+  const addToEndpoit = query ? query : "";
 
   const endpoint = "/stats" + addToEndpoit;
   const response = await axiosInstance.get<IMainStats>(endpoint);
