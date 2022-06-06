577f5b8e6d0c4b8eb6340b2b50aa61ac609547bc
Kacper Trzciński <siema311@gmail.com>
2022-06-06 20:20:10 +0200

    update stats type

--- frontend/src/interfaces/IMainStats.ts
+++ frontend/src/interfaces/IMainStats.ts
@@ -1,9 +1,16 @@
 import ISingleProjectStats from "./ISingleProjectStats";
 
+interface projectsObject {
+  lowPriority: number;
+  mediumPriority: number;
+  highPriority: number;
+  all: number;
+}
+
 export default interface IMainStats extends ISingleProjectStats {
-  allProjectsNumber: number;
-  createdProjectsNumber: number;
-  completedProjectsNumber: number;
-  updatedProjectsNumber: number;
-  allGoalsNumber: number;
+  allProjectsNumber: projectsObject;
+  createdProjectsNumber: projectsObject;
+  completedProjectsNumber: projectsObject;
+  updatedProjectsNumber: projectsObject;
+  allGoalsNumber: projectsObject;
 }
