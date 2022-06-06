1282f100bca2d74268f244f28944d0ab02fbb9e1
Kacper Trzciński <siema311@gmail.com>
2022-06-06 15:12:33 +0200

    getMainStats query

---
+++ frontend/src/hooks/query/useGetMainStats.ts
@@ -0,0 +1,13 @@
+import { useQuery } from "react-query";
+import { AxiosError } from "axios";
+import IMainStats from "../../interfaces/IMainStats";
+import IServerErrorResponse from "../../interfaces/IServerErrorResponse";
+import getMainStats from "../../api/getMainStats";
+
+export default function useGetMainStast(query?: string) {
+  return useQuery<IMainStats, Error | AxiosError<IServerErrorResponse>>(
+    ["mainStats", query],
+    () => getMainStats(query),
+    { keepPreviousData: true, staleTime: 5000 }
+  );
+}
