9a213eb12ba9b6dfa5f01c78272ce97fa03854c7
kTrzcinskii <kt.trzcinski@gmail.com>
2022-06-08 18:48:17 +0200

    stats page with required components

--- frontend/pages/statistics.tsx
+++ frontend/pages/statistics.tsx
@@ -1,6 +1,10 @@
-import { VStack } from "@chakra-ui/react";
+import { Heading, VStack } from "@chakra-ui/react";
 import type { NextPage, NextPageContext } from "next";
+import { useState } from "react";
 import Sidebar from "../src/components/sections/Sidebar";
+import GoalsStats from "../src/components/ui/statistics/GoalsStats";
+import ProjectsStats from "../src/components/ui/statistics/ProjectsStats";
+import useGetMainStats from "../src/hooks/query/useGetMainStats";
 import IMe from "../src/interfaces/IMe";
 import minHonPagesWithSidebar from "../src/utils/minHonPagesWithSidebar";
 import isUserLoggedIn from "../src/utils/server-side/isUserLoggedIn";
@@ -10,13 +14,48 @@ import setCookiesServerSide from "../src/utils/server-side/setCookiesServerSide"
 const Statistics: NextPage<{ user: IMe }> = ({ user }) => {
   const minH = minHonPagesWithSidebar;
 
+  const [query, setQuery] = useState("");
+
+  const { data, isLoading, isError } = useGetMainStats(query);
+
+  if (!data) {
+    return <>No data</>;
+  }
+
+  if (isError || isLoading) {
+    return (
+      <>
+        {isError && "error"}
+        {isLoading && "loading"}
+      </>
+    );
+  }
+
   return (
     <Sidebar>
       <VStack
         minH={minH}
         spacing={{ base: 6, md: 10, lg: 14 }}
         pt={{ base: 3, md: 6 }}
-      ></VStack>
+      >
+        <Heading color='teal.600' fontSize={{ base: "3xl", md: "4xl" }}>
+          Your statistics
+        </Heading>
+        <VStack w='full' spacing={5}>
+          <ProjectsStats
+            allProjectsNumber={data.allProjectsNumber}
+            completedProjectsNumber={data.completedProjectsNumber}
+            createdProjectsNumber={data.createdProjectsNumber}
+            updatedProjectsNumber={data.updatedProjectsNumber}
+          />
+          <GoalsStats
+            allGoalsNumber={data.allGoalsNumber}
+            completedGoalsNumber={data.completedGoalsNumber}
+            createdGoalsNumber={data.createdGoalsNumber}
+            updatedGoalsNumber={data.updatedGoalsNumber}
+          />
+        </VStack>
+      </VStack>
     </Sidebar>
   );
 };
