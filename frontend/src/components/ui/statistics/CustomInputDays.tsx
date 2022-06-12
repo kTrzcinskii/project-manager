6e00103f51e7b1d3b1354f7893f5ca81b9bd9fc1
kTrzcinskii <kt.trzcinski@gmail.com>
2022-06-12 13:09:33 +0200

    custom day input

---
+++ frontend/src/components/ui/statistics/CustomInputDays.tsx
@@ -0,0 +1,33 @@
+import { HStack, Text } from "@chakra-ui/react";
+import { Dispatch, SetStateAction, useEffect, useState } from "react";
+import CustomNumberInput from "../form/CustomNumberInput";
+
+interface CustomInputDaysProps {
+  setQuery: Dispatch<SetStateAction<string>>;
+}
+
+const CustomInputDays: React.FC<CustomInputDaysProps> = ({ setQuery }) => {
+  const [currentValue, setCurrentValue] = useState(1);
+
+  useEffect(() => {
+    setQuery(`?customFrom=${1}`);
+  }, [setQuery]);
+
+  return (
+    <HStack justifyContent='center'>
+      <Text>Last</Text>
+      <CustomNumberInput
+        defaultValue={currentValue}
+        min={1}
+        onChange={(value) => {
+          setQuery(`?customFrom=${value}`);
+          setCurrentValue(Number(value));
+        }}
+        w={currentValue < 1000 ? "80px" : "auto"}
+      />
+      <Text>{currentValue === 1 ? "day" : "days"}</Text>
+    </HStack>
+  );
+};
+
+export default CustomInputDays;
