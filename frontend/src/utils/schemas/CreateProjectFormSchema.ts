27c980d88849315ab68e74dbe500c022dd82921b
kTrzcinskii <kt.trzcinski@gmail.com>
2022-06-13 12:34:07 +0200

    create project validation schema

---
+++ frontend/src/utils/schemas/CreateProjectFormSchema.ts
@@ -0,0 +1,13 @@
+import * as Yup from "yup";
+
+const CreateProjectSchema = Yup.object().shape({
+  title: Yup.string().required("Title is required"),
+  description: Yup.string().required("Title is required"),
+  goals: Yup.array(
+    Yup.object().shape({
+      content: Yup.string().required("Content is required"),
+    })
+  ).min(1, "Your project must contain at least one goal"),
+});
+
+export default CreateProjectSchema;
