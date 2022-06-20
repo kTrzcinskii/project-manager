739c0cf613eaf12ba76ba6d96c28d3f00fc4e6e2
kTrzcinskii <kt.trzcinski@gmail.com>
2022-06-20 20:50:30 +0200

    add optional color

--- frontend/src/components/ui/project/page/DatesContainer.tsx
+++ frontend/src/components/ui/project/page/DatesContainer.tsx
@@ -5,18 +5,22 @@ interface DatesContainerProps {
   createdAt: string;
   updatedAt: string;
   completedAt: string | null;
+  color?: string;
 }
 
 const DatesContainer: React.FC<DatesContainerProps> = ({
   createdAt,
   updatedAt,
   completedAt,
+  color,
 }) => {
   return (
     <VStack w='full'>
-      <SingleDate title='Created on' date={createdAt} />
-      <SingleDate title='Updated on' date={updatedAt} />
-      {completedAt && <SingleDate title='Completed on' date={completedAt} />}
+      <SingleDate title='Created on' date={createdAt} color={color} />
+      <SingleDate title='Updated on' date={updatedAt} color={color} />
+      {completedAt && (
+        <SingleDate title='Completed on' date={completedAt} color={color} />
+      )}
     </VStack>
   );
 };
--- frontend/src/components/ui/project/page/SingleDate.tsx
+++ frontend/src/components/ui/project/page/SingleDate.tsx
@@ -4,15 +4,18 @@ import { format } from "date-fns";
 interface SingleDateProps {
   date: string;
   title: string;
+  color?: string;
 }
 
-const SingleDate: React.FC<SingleDateProps> = ({ date, title }) => {
+const SingleDate: React.FC<SingleDateProps> = ({ date, title, color }) => {
   const actualDate = new Date(date);
   const formatedDate = format(actualDate, "HH:mm, do MMMM y");
 
+  const realColor = color ? color : "teal";
+
   return (
     <HStack spacing={2} w='full'>
-      <Text fontWeight='semibold' color='teal.700'>
+      <Text fontWeight='semibold' color={`${realColor}.700`}>
         {title}:
       </Text>
       <Text color='gray.700'>{formatedDate}</Text>
