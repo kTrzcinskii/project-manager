6e24670b7a7b19765b0d336c9fb5e61c9e1c03e2
kTrzcinskii <kt.trzcinski@gmail.com>
2022-06-26 11:45:50 +0200

    fix spelling

--- frontend/src/utils/toasts/deleteProjectToastOptions.ts
+++ frontend/src/utils/toasts/deleteProjectToastOptions.ts
@@ -3,8 +3,8 @@ import { UseToastOptions } from "@chakra-ui/toast";
 const deleteProjectToastOptions = (time: number): UseToastOptions => {
   return {
     position: "top",
-    title: "Post delete!",
-    description: "Your post has been successfully deleted.",
+    title: "Project delete!",
+    description: "Your project has been successfully deleted.",
     isClosable: true,
     status: "success",
     duration: time,
