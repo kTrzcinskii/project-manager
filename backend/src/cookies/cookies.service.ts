c8a9d6fb629348236c1c6ac9c62399caa569e6b5
kTrzcinskii <kt.trzcinski@gmail.com>
2022-06-29 15:17:27 +0200

    try to fix sending cookies

--- backend/src/cookies/cookies.service.ts
+++ backend/src/cookies/cookies.service.ts
@@ -1,20 +1,27 @@
 import { Injectable } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
 import { Response } from 'express';
 import { Tokens } from 'src/auth/types';
 
 @Injectable()
 export class CookiesService {
+  constructor(private config: ConfigService) {}
+
   asignCookies(response: Response, tokens: Tokens) {
     response.cookie('at', tokens.access_token, {
       expires: new Date(new Date().getTime() + 60 * 1000 * 30), // 30 min
       sameSite: 'lax',
+      secure: true,
       httpOnly: true,
+      domain: this.config.get('FRONTEND_ORIGIN'),
     });
 
     response.cookie('rt', tokens.refresh_token, {
       expires: new Date(new Date().getTime() + 60 * 1000 * 60 * 24 * 7), // 7 days
       sameSite: 'lax',
+      secure: true,
       httpOnly: true,
+      domain: this.config.get('FRONTEND_ORIGIN'),
     });
   }
 
@@ -22,13 +29,17 @@ export class CookiesService {
     response.cookie('at', 'DELETED', {
       expires: new Date(), // expires in the moment of creation
       sameSite: 'lax',
+      secure: true,
       httpOnly: true,
+      domain: this.config.get('FRONTEND_ORIGIN'),
     });
 
     response.cookie('rt', 'DELETED', {
       expires: new Date(), // expires in the moment of creation
       sameSite: 'lax',
+      secure: true,
       httpOnly: true,
+      domain: this.config.get('FRONTEND_ORIGIN'),
     });
   }
 }
