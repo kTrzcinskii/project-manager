44f729322332606945fcb89656f570179af7911b
kTrzcinskii <kt.trzcinski@gmail.com>
2022-06-29 15:00:17 +0200

    change origin

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,12 +1,10 @@
 import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
-import { AppModule } from './app.module';
 import * as cookieParser from 'cookie-parser';
-import { ConfigService } from '@nestjs/config';
+import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  const config = app.get(ConfigService);
   app.useGlobalPipes(
     new ValidationPipe({
       whitelist: true,
@@ -14,7 +12,7 @@ async function bootstrap() {
   );
   app.enableCors({
     credentials: true,
-    origin: config.get('FRONTEND_ORIGIN'),
+    origin: '*',
   });
   app.use(cookieParser());
   await app.listen(process.env.PORT || 5000);
