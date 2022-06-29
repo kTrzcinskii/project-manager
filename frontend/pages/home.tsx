18037ec3f83b9239c9b923e623aa6f59633722e4
kTrzcinskii <kt.trzcinski@gmail.com>
2022-06-29 23:03:30 +0200

    try authenticate on client side

--- frontend/pages/home.tsx
+++ frontend/pages/home.tsx
@@ -4,16 +4,19 @@ import Head from "next/head";
 import Sidebar from "../src/components/sections/Sidebar";
 import Header from "../src/components/ui/home/Header";
 import ProjectsWrapper from "../src/components/ui/project/ProjectsWrapper";
+import useMe from "../src/hooks/query/useMe";
 import IMe from "../src/interfaces/IMe";
 import minHonPagesWithSidebar from "../src/utils/minHonPagesWithSidebar";
 import isUserLoggedIn from "../src/utils/server-side/isUserLoggedIn";
 import redirectServerSide from "../src/utils/server-side/redirectServerSide";
 import setCookiesServerSide from "../src/utils/server-side/setCookiesServerSide";
 
-const Home: NextPage<{
-  user: IMe;
-}> = ({ user }) => {
+const Home: NextPage = () => {
   const minH = minHonPagesWithSidebar;
+
+  const { data, isError, isLoading } = useMe();
+
+  if (!data || isError || isLoading) return <>todo</>;
 
   return (
     <>
@@ -22,7 +25,7 @@ const Home: NextPage<{
       </Head>
       <Sidebar>
         <VStack spacing={{ base: 5, md: 10 }} minH={minH}>
-          <Header username={user.username} />
+          <Header username={data.username} />
           <ProjectsWrapper
             title='All Projects'
             query=''
@@ -34,24 +37,24 @@ const Home: NextPage<{
   );
 };
 
-export async function getServerSideProps(ctx: NextPageContext) {
-  let cookies = ctx.req?.headers.cookie;
+// export async function getServerSideProps(ctx: NextPageContext) {
+//   let cookies = ctx.req?.headers.cookie;
 
-  if (!cookies) {
-    return redirectServerSide("/unauthorized");
-  }
+//   // if (!cookies) {
+//   //   return redirectServerSide("/unauthorized");
+//   // }
 
-  if (!cookies.includes("at=") && cookies.includes("rt=")) {
-    cookies = await setCookiesServerSide(ctx, cookies);
-  }
+//   // if (!cookies.includes("at=") && cookies.includes("rt=")) {
+//   //   cookies = await setCookiesServerSide(ctx, cookies);
+//   // }
 
-  const { logged, user } = await isUserLoggedIn(cookies);
+//   // const { logged, user } = await isUserLoggedIn(cookies);
 
-  if (!logged || !user) {
-    return redirectServerSide("/unauthorized");
-  }
+//   // if (!logged || !user) {
+//   //   return redirectServerSide("/unauthorized");
+//   // }
 
-  return { props: { user } };
-}
+//   // return { props: { user } };
+// }
 
 export default Home;
